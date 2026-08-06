aeee6513c82e6bbf400341beaf890988c16c9b16
mubasharakram786 <mubashar1418@gmail.com>
2026-08-06 17:58:19 +0500

    update password validation

--- validations/authValidation.js
+++ validations/authValidation.js
@@ -19,10 +19,10 @@ export const loginValidation = [
 
 export const passwordValidation = [
     body('password').notEmpty().withMessage('Password is Required.').isLength({min:8}).withMessage('Password must be at least 8 characters long.'),
-    body("confirmPassword").custom((value, { req }) => {
-        if (value !== req.body.password) {
-            throw new Error("Passwords do not match.");
-           }
-    return true;
-    })
+    // body("confirmPassword").custom((value, { req }) => {
+    //     if (value !== req.body.password) {
+    //         throw new Error("Passwords do not match.");
+    //        }
+    // return true;
+    // })
 ]
