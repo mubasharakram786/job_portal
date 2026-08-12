b44b9a910724c461a296e6695d4c116809c0b2d7
mubasharakram786 <mubashar1418@gmail.com>
2026-08-12 16:22:14 +0500

    update cloudinary service for upload attachment

--- services/cloudinary.service.js
+++ services/cloudinary.service.js
@@ -1,12 +1,15 @@
 import { Readable } from "stream";
 import cloudinary from "../config/cloudinary.js";
 
-export const uploadToCloudinary = async (file,folder) => {
+export const uploadToCloudinary = async (file,folder,resourceType = "raw") => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       {
-        resource_type: "raw",
-        folder
+        resource_type: resourceType,
+        folder,
+        use_filename: true,
+        unique_filename: true,
+        filename_override: file.originalname,
       },
       (error, result) => {
         if (error) {
