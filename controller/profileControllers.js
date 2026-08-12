47157337b116b248b16c71e683fd319fc2956961
mubasharakram786 <mubashar1418@gmail.com>
2026-08-12 16:24:23 +0500

    feat: update | get | apply | candidate profile routers and their controller

--- controller/profileControllers.js
+++ controller/profileControllers.js
@@ -1,7 +1,7 @@
 import Profile from '../model/profile.js'
 import { validationResult } from 'express-validator'
 import { uploadToCloudinary } from '../services/cloudinary.service.js'
-
+import JobApplication from '../model/jobApplication.js'
 export const createProfile = async(req,res,next)=>{
 const errors = validationResult(req)
     if(!errors.isEmpty()){
@@ -22,8 +22,8 @@ const errors = validationResult(req)
 export const uploadResume = async(req,res,next)=>{
     try {
         const file = req.file;
-        const uploaded = await uploadToCloudinary(file,'resume');
-        return res.status(200).json({message:"Resume Uploaded Successfully!", url:uploaded.url , publicId:uploaded.public_id, originalName:uploaded.original_filename})
+        const uploaded = await uploadToCloudinary(file,'resume','raw');
+        return res.status(200).json({message:"Resume Uploaded Successfully!", url:uploaded.url , publicId:uploaded.public_id, originalName:file.originalname})
     } catch (error) {
         return res.status(400).json({message:"Something went wrong!"})
     }
@@ -33,7 +33,65 @@ export const uploadResume = async(req,res,next)=>{
 export const uploadProfilePic = async(req,res,next)=>{
      
         const file = req.file;
-        const uploaded = await uploadToCloudinary(file,'profile');
+        const uploaded = await uploadToCloudinary(file,'profile','image');
         return res.status(200).json({message:"Profile Picture Uploaded Successfully!", url:uploaded.url})
  
+}
+
+export const applyJobApplication = async(req,res,next)=>{
+    const userId = req.userId
+    
+    const profile = await Profile.findOne({user:userId})
+    if(!profile){
+        return res.status(404).json({message:"profile_id is not found."})
+    }
+
+    try {
+        const jobApplication = new JobApplication({userId:userId, profileId:profile._id, ...req.body})
+
+        await jobApplication.save()
+
+        return res.status(201).json({message:"Job application submitted successfully", jobApplication})
+        
+    } catch (error) {
+        return res.status(500).json({message: error || "Something went wrong"})
+    }
+}
+
+export const getCandidateProfile = async(req,res,next)=>{
+    const user = req.userId;
+
+    const candidate = await Profile.findOne({user});
+
+
+    if(!candidate){
+        return res.status(404).json({message:"Candidate not found"})
+    }
+
+    return res.status(200).json({message:"Fetch candidate profile successfully", candidate})
+}
+
+export const updateProfile = async(req,res,next)=>{
+    const {id:_id} = req.params
+    try {
+        const updateProfile = await Profile.findByIdAndUpdate(_id , req.body, {new:true})
+        if(!updateProfile){
+            return res.status(400).json({message:"Unable to update the profile"})
+        }
+        return res.status(200).json({message:"Profile updated successfully"})
+    } catch (error) {
+         return res.status(500).json({message:"Something went wrong"})
+    }
+}
+
+export const candidateProfile = async(req,res,next)=>{
+    const {id} = req.params
+
+    const candidate = await Profile.findById(id);
+
+    if(!candidate){
+        return res.status(404).json({message:"Candidate not found"})
+    }
+
+    return res.status(200).json({message:"Fetch candidate profile successfully", candidate})
 }
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createProfile, uploadResume,uploadProfilePic } from '../controller/profileControllers.js'
+import { createProfile, uploadResume,uploadProfilePic,applyJobApplication,getCandidateProfile,updateProfile,candidateProfile } from '../controller/profileControllers.js'
 import { auth } from '../middleware/authMiddleware.js'
 import {imageUpload, resumeUpload} from '../middleware/upload.js'
 import { createProfileValidation } from '../validations/profileValidation.js'
@@ -10,9 +10,16 @@ const router = express.Router()
 
 router.post('/candidate-profile' , auth ,   createProfile)
 
+router.put('/update-candidate/:id' , auth ,   updateProfile)
+
+router.get('/get-candidate' , auth , getCandidateProfile)
+
 router.post('/upload-resume', auth , resumeUpload.single('resume') , uploadResume  )
 
 router.post('/upload-profile-pic', auth , imageUpload.single('profilePic'), uploadProfilePic)
 
+router.post('/apply-job', auth , applyJobApplication)
 
+// public profile
+router.get('/candidate/:id' , candidateProfile)
 export default router
