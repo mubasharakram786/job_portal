fc922e3f9061ad64ec89b26fdae1b8411dbc900b
mubasharakram786 <mubashar1418@gmail.com>
2026-08-18 17:54:35 +0500

    feat: socket.io configuration

--- server.js
+++ server.js
@@ -5,18 +5,37 @@ import userRoutes from './routes/userRoutes.js'
 import profileRoutes from './routes/profileRoutes.js'
 import companyRoutes from './routes/companyRoutes.js'
 import connection from './config/database.js'
+import { Server } from 'socket.io'
+import http from 'http'
 const app = express()
 
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors:{
+         origin:'http://localhost:3020',
+         credentials:true
+    }
+}) 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cors({
-    origin:'http://localhost:3020'
-}))
+// app.use(cors({
+//     origin:'http://localhost:3020'
+// }))
+
+
 
 app.use('/api/auth', userRoutes)
 app.use('/api', profileRoutes)
 app.use('/api', companyRoutes)
 
+
+io.on('connection', (socket)=>{
+    console.log("User connect", socket.id)
+})
+
+io.on('disconnect', ()=>{
+    console.log("User disconnected", socket.id)
+})
 
 connection()
 app.listen(process.env.PORT || 9001,()=>{
